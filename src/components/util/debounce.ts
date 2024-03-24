6b58d23c9b4a9c4402a062f54f1cde16db398668
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-03-24 15:59:07 +0900

    汎用関数追加
    debounceとthrottleを追加

---
+++ src/components/util/debounce.ts
@@ -0,0 +1,12 @@
+/**
+ * debounce
+ * 一定時間後に処理を実行する。連続して呼ばれた分は無視される。
+ * https://www.webdesignleaves.com/pr/jquery/debounce-and-throttle.html
+ */
+export function debounce(fn: Function, wait: number = 100) {
+	let timerId: number | null;
+	return (...args: unknown[]) => {
+		if (timerId) window.clearTimeout(timerId);
+		timerId = window.setTimeout(() => fn.call(null, ...args), wait);
+	};
+}
---
+++ src/components/util/throttle.ts
@@ -0,0 +1,17 @@
+/**
+ * throttle
+ * 一定の間隔で処理を実行する。呼び出された関数の実行を一定時間あたり一回に間引く。
+ * https://www.webdesignleaves.com/pr/jquery/debounce-and-throttle.html
+ */
+export function throttle(fn: Function, wait: number = 100) {
+	let isWaiting = false;
+
+	return function (...args: unknown[]) {
+		if (isWaiting) return;
+
+		isWaiting = true;
+		fn.call(null, ...args);
+
+		setTimeout(() => isWaiting = false, wait);
+	}
+}
