3f8e23712c92dca18c957c557876d39cfbcc2a06
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-03-24 21:21:47 +0900

    ページのid間違いを修正

--- src/app/development/serialize/page.tsx
+++ src/app/development/serialize/page.tsx
@@ -1,8 +1,8 @@
 import WithNavigation from "./../../../components/ui/WithNavigation/WithNavigation";
-import {MENU_ID_DEVELOPMENT} from "./../../../components/ui/WithNavigation/data";
+import {MENU_ID_SERIALIZE} from "./../../../components/ui/WithNavigation/data";
 
 export default function Home() {
 	return (
-		<WithNavigation activeMenuId={MENU_ID_DEVELOPMENT}>{""}</WithNavigation>
+		<WithNavigation activeMenuId={MENU_ID_SERIALIZE}>{""}</WithNavigation>
 	);
 }
