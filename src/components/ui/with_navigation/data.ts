afac453894051f53fa5e3b8f7ca805bf38e18823
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-06-09 21:01:16 +0900

    リファクタリング
    storybookの導入
    ディレクトリ構成の変更
    不要ファイルの削除

--- src/components/ui/with_navigation/data.ts
+++ src/components/ui/with_navigation/data.ts
@@ -1,7 +1,5 @@
-import { Setting } from "./types";
-import { lang } from "@/lib/lang/lang";
-
-//@@todo 別の場所に定義する
+import { Setting } from '@/app/_types';
+import { lang } from '@/lib/lang/lang';
 
 export const MENU_ID_HOME: number = 1;
 export const MENU_ID_DEVELOPMENT: number = 2;
@@ -19,11 +17,11 @@ export const navigationData: Setting[] = [
     id: MENU_ID_DEVELOPMENT, label: lang('0009'), icon: 'Code', path: '/development', children: [//開発
       {id: MENU_ID_BASE64, label: lang('0010'), path: '/development/base64', children: []},//base64 encode/decode
       {id: MENU_ID_SERIALIZE, label: lang('0011'), path: '/development/serialize', children: []},//PHP serialize/unserialize
-    ]
+    ],
   },
   {
     id: MENU_ID_WORK, label: lang('0013'), icon: 'Work', path: '/work', children: [//一般業務
       {id: MENU_ID_TEMPLATE_MAKER, label: lang('0014'), path: '/work/template_maker', children: []},//定型文生成
-    ]
+    ],
   },
 ];
