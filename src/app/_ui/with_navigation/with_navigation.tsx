9204e21ef2ac3b4d24b98ae501ccb8f019cc377a
T-Ageishi <takuto.ageishi.dev@gmail.com>
2024-06-17 22:42:05 +0900

    レイアウト・ナビゲーション更新

--- src/app/_ui/with_navigation/with_navigation.tsx
+++ src/app/_ui/with_navigation/with_navigation.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import NavigationRail from '@/app/_ui/navigation_rail/navigation_rail';
+import { Setting } from '@/app/_types';
 
-export default function WithNavigation ({children}: { children: React.ReactNode }) {
+export default function WithNavigation ({
+  children, navigationData,
+}: { children: React.ReactNode; navigationData: Setting[] }) {
   return (
     <>
+      <NavigationRail settings={navigationData}/>
       {children}
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { navigationData } from '@/app/_ui/_lib/navigation/data';
 import WithNavigation from '@/app/_ui/with_navigation/with_navigation';
+import Description from '@/app/_ui/description/description';
+import React from 'react';
 
 export default function Home () {
   return (
-    <WithNavigation>
-      <h1>Home</h1>
+    <WithNavigation navigationData={navigationData}>
+      <main className={'page-main-content'}>
+        <Description heading={'Home'} description={''}/>
+      </main>
     </WithNavigation>
   );
 }
